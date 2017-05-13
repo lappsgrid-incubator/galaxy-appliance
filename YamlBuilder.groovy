1f41081e3b85d46b97eca9f7e930f4c664d6c72e
Keith Suderman <suderman@cs.vassar.edu>
2017-05-13 11:20:25 -0400

    Added parameter to mount an /export volume for Galaxy.

--- YamlBuilder.groovy
+++ YamlBuilder.groovy
@@ -3,8 +3,18 @@
  * file needed to run all the Docker containers that make up a
  * LAPP Grid Galaxy Appliance.
  */
- 
-if (args.size() == 0) {
+@Grab('commons-cli:commons-cli:1.2')
+
+CliBuilder cli = new CliBuilder()
+cli.usage = 'groovy YamlBuilder.groovy [-e path] <repository> module [module ...]'
+cli.header = '\nGenerates the docker-compose.yml file for a Galaxy appliance.\n'
+cli.footer = '\nCopyright 2017 The Language Applications Grid.\n'
+cli.h(longOpt:'help', 'displays this help message')
+cli.e(longOpt:'export', args:1, 'define the directory to mount as the /export volume for Galaxy')
+
+def params = cli.parse(args) 
+if (args.size() == 0 || params.h) {
+	/*
 	println '''
 USAGE 
     groovy YamlBuilder.groovy <repository name> module [module ...]
@@ -13,22 +23,15 @@ EXAMPLE
     groovy YamlBuilder.groovy lappsgrid masc oaqa lingpipe gate stanford
     
 '''
+	*/
+	cli.usage()
 	return
 }
 
-String appliance = args[0]
-args = args[1..-1]
+String appliance = params.arguments()[0]
+args = params.arguments()[1..-1]
 
 int port = 8000
-/***
-println "docker network create -d overlay --subnet 192.168.0.0/16 appliance-network"
-args.each {
-	println "docker service create --name $it -p ${++port}:8080/tcp --network appliance-network lappsgrid/$it"
-}
-println "docker service create --name galaxy -p 80:80/tcp --network appliance-network lappsgrid/galaxy-keith"
-
-return
-*/
 
 println "version: '2'"
 println "services:"
@@ -41,9 +44,13 @@ args.each {
 }
 
 println """    galaxy:
-        image: $appliance/galaxy-keith
-        build: ./build
-        ports:
+        image: lappsgrid/galaxy-appliance
+        build: ./build"""
+        if (params.e) {
+        	println "        volumes:"
+        	println "            - ${params.e}:/export/"
+        }
+println """        ports:
             - "80:80"
         depends_on:"""
 args.each {
